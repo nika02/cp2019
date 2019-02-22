6d3377acdd474070d3384a3e1db9161430e5c343
nika02 <47800353+nika02@users.noreply.github.com>
2019-02-23 00:10:04 +0800

    Create q1_fahrenheit_to_celsius.py

---
+++ q1_fahrenheit_to_celsius.py
@@ -0,0 +1,9 @@
+# Write a program q1_fahrenheit_to_celsius.py that reads a Fahrenheit degree
+# in double (floating point / decimal) from standard input, then converts
+# it to Celsius and displays the result in standard output.
+# The formula for the conversion is as follows: celsius = (5/9) * (fahrenheit - 32)
+
+Input = input("Enter Fahrenheit: ")
+Fahrenheit = int(Input)
+Celsius = (5/9) * (round(Fahrenheit, 2) - 32)
+print(Celsius)
