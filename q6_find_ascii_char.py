75a207a387105c1a0a19e6d3cc47bccdb1e9b239
nika02 <47800353+nika02@users.noreply.github.com>
2019-02-23 20:59:01 +0800

    Create q6_find_ascii_char.py

---
+++ q6_find_ascii_char.py
@@ -0,0 +1,5 @@
+# Write a program that receives an ASCII code (an integer between 0 and 127) and displays its character.
+# For example, if the user enters 97, the program displays character a.
+
+Input = int(input("Enter: "))
+print(chr(Input))
