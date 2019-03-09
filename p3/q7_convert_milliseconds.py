8c6576294b5c46dd61cef390de868d1537c78c3e
nika02 <47800353+nika02@users.noreply.github.com>
2019-03-09 17:09:33 +0800

    Create q7_convert_milliseconds.py

---
+++ p3/q7_convert_milliseconds.py
@@ -0,0 +1,12 @@
+def convert_ms():
+    millis = input("Enter time in milliseconds: ")
+    millis = int(millis)
+    seconds = (millis/1000)%60
+    seconds = int(seconds)
+    minutes = (millis/(1000*60))%60
+    minutes = int(minutes)
+    hours = (millis/(1000*60*60))%24
+
+    print ("%d:%d:%d" % (hours, minutes, seconds))
+
+convert_ms()
